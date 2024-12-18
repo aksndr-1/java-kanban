27570f26353f8aca4feb91793a435661a6315b83
Arzamastsev <aa@terralink-global.com>
2024-12-19 01:27:00 +0300

    fix: CheckStyle исправление по замечанию автопроверки

--- src/ru/aksndr/exceptions/IdNotANumberException.java
+++ src/ru/aksndr/exceptions/IdNotANumberException.java
@@ -1,6 +1,6 @@
 package ru.aksndr.exceptions;
 
-public class IdNotANumberException extends Exception{
+public class IdNotANumberException extends Exception {
     public IdNotANumberException(String message) {
         super(message);
     }
